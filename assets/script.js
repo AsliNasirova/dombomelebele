481558fd5344708a1bae9496717b77c1e0b20cc2
tu83bnh9m@code.edu.az <tu83R2GJ2@code.edu.az>
2023-10-24 13:45:42 +0400

    fnjn

---
+++ assets/script.js
@@ -0,0 +1,80 @@
+// let div=document.createElement('div')
+// let btn=document.createElement('button')
+// let body=document.querySelector('body')
+
+// div.style.width='200px'
+// div.style.height='200px'
+// div.style.backgroundColor='red'
+
+// body.append(div)
+// body.append(btn)
+
+// btn.onclick=()=>{
+//     div.style.width='400px'
+//     div.style.height='400px'
+// }
+
+// task1
+// let SABAH=document.getElementById('SABAH')
+// console.log(SABAH);
+
+// task3
+// let div=document.createElement('div')
+// div.style.width="600px";
+// div.style.height="600px";
+// div.style.backgroundColor="violet"
+
+// let body=document.querySelector('body')
+// body.append(div)
+// div.onclick=()=>{
+//     div.style.width="700px";
+//     div.style.height="600px";
+
+//     div.style.backgroundColor="violet"
+// }
+
+// task4
+// let div=document.querySelector('divv')
+// document.addEventListener('click', function handleClick(event) {
+//     event.target.classList.add('bg-yellow');
+//   });
+
+//   document.addEventListener('click', function handleClick(event) {
+//     console.log('user clicked: ', event.target);
+  
+//     event.target.classList.add('bg-yellow');
+//   });
+
+// task 5
+// document.querySelector('.target').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     var target = document.querySelector('.target');
+//     if (target.classList.contains('active')) {
+//       target.classList.remove('active');
+//     } else {
+//       target.classList.add('active');
+//     }
+//   });
+
+//   document.addEventListener('click', function handleClick(event) {
+//     console.log('user clicked: ', event.target);
+  
+//     event.target.classList.add('bg-yellow');
+//   });
+
+// function myFunction() {
+//     var element = document.getElementById("divv");
+//     element.classList.toggle("divv");
+//   }
+
+// function setHeight() {
+//     if (content.offsetHeight) {
+//       content.style.height = 0;
+//     } else {
+//       content.style.height = accordText.offsetHeight + 'px';
+//     };
+//   };
+
+// task 6
+let divv=document.querySelector('divv')
+alert
